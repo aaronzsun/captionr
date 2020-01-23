544a415dbbdda8b63163e5673b316c981633b706
CharlesOnesti <35116629+CharlesOnesti@users.noreply.github.com>
2020-01-22 23:28:50 -0500

    feed page stuff

---
+++ bootcamp-web-starter-master/src/containers/Home/Display_Images/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {useQuery} from '@apollo/react-hooks'
+import GET_ALL_IMAGES from './graphql'
+
+
+
+const Display = () => {
+    const { loading, error, data } = useQuery(GET_ALL_IMAGES)
+    if(loading) return 'loading...'
+    if(error) return 'Oops! no images!'
+    console.log(data)
+
+    return(
+    <div>
+        {data.map((img) => {
+            <img src={img.url} />
+        })}
+    </div>
+    )
+}
+
+export default Display
--- bootcamp-web-starter-master/src/containers/Home/index.js
+++ bootcamp-web-starter-master/src/containers/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import FadeIn from 'react-fade-in'
 import { Title } from './styles'
+import Display from './Display_Images'
 
 const Home = () => (
   <FadeIn>
     <Title> FEED </Title>
+    <Display/>
   </FadeIn>
 )
 
